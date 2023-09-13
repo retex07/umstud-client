5069e66d22961580251e639a66ad30cabf9e2308
Wilfaded <67386742+retex07@users.noreply.github.com>
2023-09-13 18:58:26 +0700

    feat: Add inlineLoader

---
+++ src/components/loaders/inlineLoader/component.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.scss";
+
+/**
+ * {@link https://loading.io/css/ Source (lds-ring)}
+ */
+export default function InlineLoader() {
+  return (
+    <div className="inline-loader">
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+}
