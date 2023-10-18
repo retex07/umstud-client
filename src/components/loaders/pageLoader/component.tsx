796990be0c0b8845ab580b896ea92feedf3b6c71
Wilfaded <67386742+retex07@users.noreply.github.com>
2023-10-18 16:01:32 +0700

    feat: Add PageLoader for pages

---
+++ src/components/loaders/pageLoader/component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const PageLoader = () => {
+  return (
+    <div className="wrapper">
+      <div className="loader" />
+    </div>
+  );
+};
+
+export default PageLoader;
