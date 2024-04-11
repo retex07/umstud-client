d198d8c891ac77abe2af3b9e0e82d3ecf837fe44
Wilfaded <67386742+retex07@users.noreply.github.com>
2024-04-11 20:45:07 +0700

    refactor: Update Menu User component

--- src/components/menuUser/component.tsx
+++ src/components/menuUser/component.tsx
@@ -1,13 +1,12 @@
-import { baseLinkMedias, linkMediaFiles } from "constants/config";
-
 import { Dispatch } from "@reduxjs/toolkit";
+import { baseUrl as baseUrlProfile } from "pages/profile/routes";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { ReactComponent as ExampleAvatarSvg } from "static/images/example-avatar.svg";
 import { actions as userActions } from "store/user";
 import { user as user_selector } from "store/user/user.selectors";
-import { replaceSubstringLink } from "utils/link.utils";
 import "./styles.scss";
 
 interface Props {
@@ -17,35 +16,67 @@ interface Props {
 
 export default function MenuUser(props: Props) {
   const { isOpen, onHide } = props;
+  const { t } = useTranslation("p_profile");
   const { user } = useSelector(user_selector);
   const dispatch = useDispatch<Dispatch>();
-  const history = useHistory();
+  const location = useLocation();
 
-  const avatarUrl = replaceSubstringLink(
-    user?.photo || "",
-    baseLinkMedias,
-    linkMediaFiles
-  );
+  const items = [
+    {
+      route: "/",
+      title: t("index.title"),
+    },
+    {
+      route: "/messages",
+      title: t("messages.title"),
+    },
+    {
+      route: "/ready-tasks",
+      title: t("readyTasks.title"),
+    },
+    {
+      route: "/black-list",
+      title: t("blackList.title"),
+    },
+    {
+      route: "/orders",
+      title: t("orders.title"),
+    },
+    {
+      route: "/works",
+      title: t("works.title"),
+    },
+  ];
+
+  function renderAvatar() {
+    switch (true) {
+      case user && !!user.photo: {
+        return <img src={user?.photo || ""} alt={user?.username} />;
+      }
+      default:
+        return <ExampleAvatarSvg />;
+    }
+  }
 
   function onLogout() {
     dispatch(userActions.logout());
   }
 
-  if (!user?.photo) {
-    return <ExampleAvatarSvg />;
-  }
-
   return (
     <div className="menu-user" onClick={onHide}>
-      <img src={avatarUrl} alt={user?.username} />
+      {renderAvatar()}
       {isOpen && (
         <div className="menu-user__wrapper">
-          <label
-            className="menu-user__item"
-            onClick={() => history.push("/profile")}
-          >
-            Профиль
-          </label>
+          {!location.pathname.includes(baseUrlProfile) &&
+            items.map((item, index) => (
+              <Link
+                key={index}
+                className="menu-user__item"
+                to={baseUrlProfile + item.route}
+              >
+                {item.title}
+              </Link>
+            ))}
           <label className="menu-user__item red" onClick={onLogout}>
             Выйти
           </label>
