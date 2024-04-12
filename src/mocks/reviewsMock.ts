3b922ff1a219284e0eb3e29716e0649a8131cf5b
Wilfaded <67386742+retex07@users.noreply.github.com>
2024-04-13 00:51:59 +0700

    feat: To make up the missing design of the main page

--- src/mocks/reviewsMock.ts
+++ src/mocks/reviewsMock.ts
@@ -1,8 +1,37 @@
 export const ReviewsMock = [
   {
+    id: 0,
     date: "3 марта 2023",
-    author: "Александр",
+    author: "Егор",
     comment: "Слишком дорогой сервис!",
     countStars: 4,
+  },
+  {
+    id: 1,
+    date: "12 апреля 2024",
+    author: "Александр",
+    comment: "Я устал фиксить баги!",
+    countStars: 1,
+  },
+  {
+    id: 2,
+    date: "1 января 2024",
+    author: "Олег",
+    comment: "Всё нравится",
+    countStars: 5,
+  },
+  {
+    id: 3,
+    date: "12 апреля 2024",
+    author: "Маргарита",
+    comment: "Статью возвращают сотый раз!",
+    countStars: 5,
+  },
+  {
+    id: 4,
+    date: "1 сентября 2023",
+    author: "Михаил",
+    comment: "Только не фриланс-биржа...",
+    countStars: 5,
   },
 ];
