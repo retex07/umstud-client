b0b37bd35acc2bb050f90350daca14cd01bad796
alexandr.taushkanov <alexandr.taushkanov@citeck.ru>
2025-06-02 21:13:13 +0700

    feat: Add checkbox and fieldCheckbox components

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,53 @@
+import classNames from "classnames";
+import React, { HTMLAttributes, useState } from "react";
+
+import { ReactComponent as CheckSvg } from "@/static/images/check.svg";
+import "./Checkbox.scss";
+
+export interface CheckboxProps
+  extends Omit<HTMLAttributes<HTMLInputElement>, "onChange"> {
+  label?: string;
+  disabled?: boolean;
+  onChange?: (checked: boolean) => void;
+  classNameWrapper?: string;
+}
+
+export default function Checkbox({
+  label,
+  defaultChecked = false,
+  onChange,
+  disabled,
+  className,
+  classNameWrapper,
+  ...checkboxAttributes
+}: CheckboxProps) {
+  const [checked, setChecked] = useState(defaultChecked);
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setChecked(e.target.checked);
+    onChange && onChange(e.target.checked);
+  }
+
+  return (
+    <label className={classNames("umstud-checkbox", classNameWrapper)}>
+      <input
+        {...checkboxAttributes}
+        className="umstud-checkbox_hidden"
+        hidden
+        type="checkbox"
+        disabled={disabled}
+        checked={checked}
+        onChange={handleChange}
+      />
+      <div
+        className={classNames("umstud-checkbox__field", className, {
+          disabled,
+          active: checked,
+        })}
+      >
+        {checked && <CheckSvg />}
+      </div>
+      {label && <span className="umstud-checkbox_span">{label}</span>}
+    </label>
+  );
+}
---
+++ src/components/formElements/checkboxField/CheckboxField.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Control,
+  FieldValues,
+  Path,
+  PathValue,
+  UnpackNestedValue,
+  useController,
+  UseControllerProps,
+} from "react-hook-form";
+
+import Checkbox from "@/components/Checkbox";
+import { CheckboxProps } from "@/components/Checkbox/Checkbox";
+
+interface Props<T extends FieldValues>
+  extends Omit<CheckboxProps, "defaultValue"> {
+  name: Path<T>;
+  control: Control<T>;
+  rules?: UseControllerProps["rules"];
+  defaultValue?: UnpackNestedValue<PathValue<T, Path<T>>>;
+  readOnly?: boolean;
+}
+
+export default function CheckboxField<FormField extends FieldValues>(
+  props: Props<FormField>
+) {
+  const { control, name, rules, defaultValue, ...rest } = props;
+
+  const { field } = useController({
+    control: control,
+    name: name,
+    rules: rules,
+    defaultValue: defaultValue,
+  });
+
+  return <Checkbox {...field} {...rest} defaultChecked={defaultValue} />;
+}
